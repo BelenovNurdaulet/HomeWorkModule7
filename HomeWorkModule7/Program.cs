6b614da438e8647555868bd11d7bc85a0f8e841c
Нурдаулет Беленов <belenovnurdaulet179@gmail.com>
2023-10-22 22:52:28 +0600

    Добавьте файлы проекта.

---
+++ HomeWorkModule7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLib.WordOfTanks;
+
+namespace Day7_Tanks
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    Console.WriteLine($"Бой №{i}:");
+
+                    Tank t34 = new Tank("Т-34");
+                    Tank pantera = new Tank("Pantera");
+
+                    Console.WriteLine(t34);
+                    Console.WriteLine(pantera);
+
+                    if (t34 ^ pantera)
+                    {
+                        Console.WriteLine($"Т-34 побеждает над Pantera!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Pantera побеждает над Т-34!");
+                    }
+
+                    Console.WriteLine(); // Добавляем пустую строку для разделения симуляций
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
---
+++ HomeWorkModule7/WOT/Tank.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib.WordOfTanks
+{
+   
+        public class Tank
+        {
+            private string tankName;
+            private int ammunition;
+            private int armorLevel;
+            private int maneuverability;
+
+            public Tank(string name)
+            {
+                tankName = name;
+                List<int> uniqueValues = GetUniqueValues();
+                ammunition = uniqueValues[0];
+                armorLevel = uniqueValues[1];
+                maneuverability = uniqueValues[2];
+            }
+
+            private List<int> GetUniqueValues()
+            {
+                List<int> values = Enumerable.Range(0, 101).OrderBy(x => Guid.NewGuid()).ToList();
+                // Перетасовка уникальных значений от 0 до 100
+                return values.Take(3).ToList();
+            }
+
+            public override string ToString()
+        {
+            return $"{tankName} - Боекомплект: {ammunition}%, Уровень брони: {armorLevel}%, Уровень маневренности: {maneuverability}%";
+        }
+
+        public static bool operator ^(Tank tank1, Tank tank2)
+        {
+            int score = 0;
+            if (tank1.ammunition > tank2.ammunition)
+                score++;
+            if (tank1.armorLevel > tank2.armorLevel)
+                score++;
+            if (tank1.maneuverability > tank2.maneuverability)
+                score++;
+
+            return score >= 2;
+        }
+    }
+}
